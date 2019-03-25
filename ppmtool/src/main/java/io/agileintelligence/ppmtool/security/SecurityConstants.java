915b14ac302a22373c4e385b7b793c67072120a6
sushmakayem <37783638+sushmakayem@users.noreply.github.com>
2019-03-25 09:21:42 -0700

    complete project

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
@@ -8,5 +8,5 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX= "Bearer ";//has to be space here
     //because when we push it to the server it needs that space before the token id is added with it
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 200_000; // 2minutes
+    public static final long EXPIRATION_TIME = 2000_000; // 2minutes
 }
